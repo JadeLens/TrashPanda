ec53364cdccedb64c6f47c6feee3dbb1c9b1332d
raimuriel <jarrettjdeans@gmail.com>
2016-02-08 09:34:00 -0500

    Jarrett's Merge (Resource Management)

---
+++ Assets/JarrettAssets/Scripts/PlayerResources.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//use this script on an empty object to hold the players resources
+public class PlayerResources : MonoBehaviour
+{
+        private int Water = 0;
+        private int Trash = 0;
+
+        public int getTrash()
+        {
+            return Trash;
+        }
+
+        public int getWater()
+        {
+            return Water;
+        }
+
+        public void IncrementTrash(int x)
+        {
+            Trash += x;
+        }
+
+        public void IncrementWater(int x)
+        {
+            Water += x;
+        }
+}
---
+++ Assets/JarrettAssets/Scripts/ResourceCollisionHandling.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourceCollisionHandling : MonoBehaviour {
+
+    public PlayerResources myResources; //the player controlling this animal
+    public int IncrementationTrash = 0;
+    public int IncrementationWater = 0;
+
+    //put this script on the animal to handle the resource collecting when collision
+    //also link the PlayerResources object in the game to the owner of the animal.
+    void OnTriggerEnter(Collider other)
+    {
+        if (myResources != null)
+        {
+            CollectResources Resource = (CollectResources)other.gameObject.GetComponent(typeof(CollectResources));
+            IncrementationTrash = Resource.getIncTrash();
+            IncrementationWater = Resource.getIncWater();
+
+            if (other.gameObject.CompareTag("Trash"))
+            {
+                myResources.IncrementTrash(IncrementationTrash);
+                if (Resource.getDestroyable() == true)
+                    Destroy(other.gameObject);
+            }
+            else if (other.gameObject.CompareTag("Water"))
+            {
+                myResources.IncrementWater(IncrementationWater);
+                if (Resource.getDestroyable())
+                    Destroy(other.gameObject);
+            }
+            else if (other.gameObject.CompareTag("Resource"))
+            {
+                myResources.IncrementTrash(IncrementationTrash);
+                myResources.IncrementWater(IncrementationWater);
+                if (Resource.getDestroyable())
+                    Destroy(other.gameObject);
+            }
+        }
+    }
+}
