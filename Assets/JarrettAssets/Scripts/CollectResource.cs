4bcc5a25785f96d39cea6f7ee8c17937d1fd7697
raimuriel <jarrettjdeans@gmail.com>
2016-02-09 14:15:54 -0500

    Added a simple Singleplayer Base Collection script. new problem of the AI doesn't like triggers anymore and can't travel through them. (was there before I changed anything)

---
+++ Assets/JarrettAssets/Scripts/CollectResource.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//use this script on objects to collect resources
+public class CollectResource : MonoBehaviour
+{
+    public bool bDestroyOnCollision = true; //Destroy the resource once collected
+   
+    public int incWater = 0; //incrementation of Water to player
+    public int incTrash = 0; //incrementation of Trash to player
+    public int MaxTrash = 0; //Max Trash a Base Provides
+    public int MaxWater = 0; //Max Water a Base Provides
+
+    public bool getDestroyable()
+    {
+        return bDestroyOnCollision;
+    }
+
+    public int getIncWater()
+    {
+        return incWater;
+    }
+    public int getIncTrash()
+    {
+        return incTrash;
+    }
+}
