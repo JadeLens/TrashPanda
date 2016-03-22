a2a153641503720972ac45ab885cfc049caa4cb4
raimuriel <jarrettjdeans@gmail.com>
2016-03-22 14:58:49 -0400

    Simple Fog of War with Line of Sight.
    Applied a Layer name to all the Enemies and Players to "Enemy" & "Player".
    added a script to the enemy AI
    added a script to the players
    added a ball of light to the players.
    added a shader to the canvas for a fog of war effect.
    
    Enemies no long render when out of the line of sight from players.
    Note: This probably won't work if it's a 1v1 but when its 1vs AI its fine.
    
    To Do:
    Add this light to occupied buildings.

---
+++ Assets/JarrettAssets/Scripts/FogOfWarSight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Put this on the Player Object
+public class FogOfWarSight : MonoBehaviour
+{
+    public float radius = 5.0f;
+    public LayerMask layerMask = -1;
+
+	// Use this for initialization
+	void Start ()
+    {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    foreach(Collider col in Physics.OverlapSphere(transform.position, radius, layerMask))
+        {  
+            if (col.gameObject.GetComponent<Renderer>() != null)
+            {
+                col.SendMessage("Observed", SendMessageOptions.DontRequireReceiver);
+                //col.gameObject.GetComponent<Renderer>().enabled = true;
+            }
+        }
+	}
+}
---
+++ Assets/JarrettAssets/Scripts/FogOfWarVisibility.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Put this on the Enemy
+public class FogOfWarVisibility : MonoBehaviour
+{
+    private bool observed = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if (observed)
+        {
+            //this.gameObject.GetComponent<Renderer>().enabled = true;
+            this.gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
+            this.gameObject.GetComponentInChildren<Canvas>().enabled = true;
+        }
+        else
+        {
+            //this.gameObject.GetComponent<Renderer>().enabled = false;
+            this.gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false;
+            this.gameObject.GetComponentInChildren<Canvas>().enabled = false;
+        }
+
+        observed = false;
+	}
+
+    void Observed()
+    {
+        observed = true;
+    }
+}
