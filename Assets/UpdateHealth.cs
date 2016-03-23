9546292bf8553be6531528f89986cec286ca9b94
Ryan Hammond <ryan.hammond33@gmail.com>
2016-03-23 11:10:38 -0400

    New scene with fog of war, unit selection, health bars working

---
+++ Assets/UpdateHealth.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateHealth : MonoBehaviour
+{
+
+    public Slider healthBar;
+    private UnitStats_ForRTS stats;
+
+    // Use this for initialization
+    void Start()
+    {
+        stats = GetComponent<UnitStats_ForRTS>();
+        healthBar = GetComponentInChildren<Slider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        healthBar.value = stats.getCurrentHealth();
+
+
+    }
+}
+
