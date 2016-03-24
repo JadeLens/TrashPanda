dbc16051ab751b2c5f1fa945f0676808eda5f000
poboivin <poboivin@hotmail.com>
2016-03-24 17:11:03 -0400

    increased performance by removing character controller changed movement code accordingly my laptop can now support 50 units at 30fp  compared to 15 units at 30fps

--- Assets/PierAssets/scripts/Pier_Unit.cs
+++ Assets/PierAssets/scripts/Pier_Unit.cs
@@ -1,25 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 using Pathfinding;
+
 public class Pier_Unit : MonoBehaviour {
 
 
-    public Vector3 targetPosition;
-    public Vector3 castPosition;
-    public Vector3 tP;
+   // public Vector3 targetPosition;
+    //public Vector3 castPosition;
+    //public Vector3 tP;
 
     public Seeker seeker;
     public rabbitAnim rAnim;
     public unitSelection unitS;
-    public CharacterController controller;
+
+
+    public float separationCoeficient = 60;
+    public float separationRadius = 1;
+    public float destinationCoeficient = 25;
+    //public CharacterController controller;
     public Path path;
 
     Ray camRay;
     RaycastHit camRayHit;
 
-    public float nextWaypointDistance = 0.1f;
+    public float nextWaypointDistance = 1f;
 
-    public int speed = 200;
+    public int speed = 5;
     private int currentWaypoint = 0;
 
     public LayerMask layerItem;
@@ -36,7 +42,7 @@ public class Pier_Unit : MonoBehaviour {
     public void Start()
     {
         seeker = GetComponent<Seeker>();
-        controller = GetComponent<CharacterController>();
+    //    controller = GetComponent<CharacterController>();
     //    rAnim = GameObject.Find("RABBIT").GetComponent<rabbitAnim>();
     //    unitS = GameObject.Find("RABBIT/RABBIT").GetComponent<unitSelection>();
 
@@ -66,25 +72,101 @@ public class Pier_Unit : MonoBehaviour {
             lookat = true;
         }
     }
+    //void part1()
+    //{
 
+    //    camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+    //    if (Physics.Raycast(camRay, out camRayHit))
+    //    {
+    //        if (Input.GetMouseButtonDown(1))
+    //        {
+    //            lookat = true;
+    //            if (camRayHit.transform.gameObject.layer == layerItem)
+    //            {
+    //                targetPosition = new Vector3(camRayHit.point.x, 0.0f, camRayHit.point.z);
+    //            }
+    //            //  seeker.StartPath(transform.position, targetPosition, OnPathComplete);
+    //        }
+    //    }
+    //}
+
+    Vector3 Seperation()
+    {
+        // seperation behaviour steers boids into opposite directions of their neighbours
+        Vector3 r = new Vector3();
+        // get all neghbours in radius R
+        var neighbours = RTSUnitManager.GetUnitList(); //world.GetNeighbours(this, config.Rseperation);
+        // if no neighboiurs then no seperation needed
+        if (neighbours.Count == 0)
+        {
+            return r;
+        }
+        // adding contibuting vectors of neghbours towarsds this boid
+        foreach (var agent in neighbours)
+        {
+            // checking for field of vision boids
+            if (Vector3.Distance(agent.transform.position, this.transform.position) < separationRadius)
+            {
+                Vector3 towardsMe = this.transform.position - agent.transform.position;
+
+                if (towardsMe.magnitude > 0)
+                {
+                    // force that each agent has on me is inversely proportional to the distance
+                    r += towardsMe.normalized / towardsMe.magnitude;
+
+                }
+
+            }
+        }
+
+        return r.normalized;
+    }
+    Vector3 Destination()
+    {
+        return   (path.vectorPath[currentWaypoint] - transform.position).normalized;
+
+
+    }
+    void part2Math()
+    {
+   //     Debug.Log(currentWaypoint);
+      
+
+
+        Vector3 r = separationCoeficient * Seperation() + destinationCoeficient * Destination();
+                   ;
+
+        //   dir *= speed * Time.deltaTime;
+        //     controller.SimpleMove(dir);
+        r = r.normalized * speed * Time.deltaTime;
+        r = new  Vector3(r.x, 0, r.z);
+        transform.position += r;
+     //   transform.Translate(dir * speed * Time.deltaTime,Space.World);
+    //  transform.position = path.vectorPath[currentWaypoint];
+        //  tP = new Vector3(0.0f, path.vectorPath[currentWaypoint].y, path.vectorPath[currentWaypoint].z);
+
+        //Vector3 targetDir = path.vectorPath[currentWaypoint] - transform.position;
+        //float step = speed * Time.deltaTime;
+        //Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
+
+        if (lookat)
+        {
+            //  transform.LookAt(path.vectorPath[currentWaypoint]);
+
+            Vector3 relativePos = path.vectorPath[currentWaypoint] - new Vector3(transform.position.x, path.vectorPath[currentWaypoint].y, transform.position.z);
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            transform.rotation = rotation;
+            //  StartCoroutine(lookTowards(0.1f));
+            lookat = false;
+        }
+
+    }
     public void FixedUpdate()
     {
 
-
-        camRay = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-        if (Physics.Raycast(camRay, out camRayHit))
-        {
-            if (Input.GetMouseButtonDown(1))
-            {
-                lookat = true;
-                if (camRayHit.transform.gameObject.layer == layerItem)
-                {
-                    targetPosition = new Vector3(camRayHit.point.x, 0.0f, camRayHit.point.z);
-                }
-              //  seeker.StartPath(transform.position, targetPosition, OnPathComplete);
-            }
-        }
+     //   part1();
+      
 
         if (path == null)
         {
@@ -95,32 +177,12 @@ public class Pier_Unit : MonoBehaviour {
         {
             stationary = true;
             rAnim.anim.CrossFade("idle1");
-           // Debug.Log("End Of Path Reached");
+            // Debug.Log("End Of Path Reached");
             return;
         }
-
-        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
-
-
-
-
-        dir *= speed * Time.deltaTime;
-        controller.SimpleMove(dir);
-      //  tP = new Vector3(0.0f, path.vectorPath[currentWaypoint].y, path.vectorPath[currentWaypoint].z);
-
-        Vector3 targetDir = path.vectorPath[currentWaypoint] - transform.position;
-        float step = speed * Time.deltaTime;
-        Vector3 newDir = Vector3.RotateTowards(transform.forward, targetDir, step, 0.0f);
-
-        if (lookat)
+        else
         {
-          //  transform.LookAt(path.vectorPath[currentWaypoint]);
-
-            Vector3 relativePos = path.vectorPath[currentWaypoint] - new Vector3(transform.position.x, path.vectorPath[currentWaypoint].y, transform.position.z);
-            Quaternion rotation = Quaternion.LookRotation(relativePos);
-            transform.rotation = rotation;
-            StartCoroutine(lookTowards(0.1f));
-
+            part2Math();
         }
         if (stationary)
         {
@@ -131,7 +193,7 @@ public class Pier_Unit : MonoBehaviour {
             lookat = true;
             stationary = false;
         }
-        if (!stationary)
+        if (!stationary )
         {
             rAnim.anim.Play("run");
         }
@@ -143,6 +205,7 @@ public class Pier_Unit : MonoBehaviour {
         if (Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]) < nextWaypointDistance)
         {
             currentWaypoint++;
+    //        Debug.Log("next Pt");
             lookat = true;
             return;
 
@@ -150,11 +213,11 @@ public class Pier_Unit : MonoBehaviour {
 
     }
 
-    void LateUpdate()
-    {
+    //void LateUpdate()
+    //{
 
 
-        Debug.DrawRay(camRay.origin, camRay.direction * 50, Color.green);
+    //    Debug.DrawRay(camRay.origin, camRay.direction * 50, Color.green);
 
-    }
+    //}
 }
