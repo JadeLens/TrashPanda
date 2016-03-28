e71d9f0d41b724f81e4a96754afc2e984f446263
Steven Lambert <jadedeath@gmail.com>
2016-03-28 09:23:57 -0400

    added woodland animals pack

---
+++ Assets/woodlandAnimalPack/rabbitDemoFunctions.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rabbitDemoFunctions : MonoBehaviour {
+	public GameObject rabbit;
+	public Material brown;
+	public Material white;
+
+
+	// Use this for initialization
+	void BrownMaterial () {
+		rabbit.GetComponent<Renderer>().material = brown;
+	}
+	
+	// Update is called once per frame
+	void WhiteMaterial() {
+		rabbit.GetComponent<Renderer>().material = white;
+	}
+}
---
+++ Assets/woodlandAnimalPack/universalDemoGUI.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class universalDemoGUI : MonoBehaviour {
+	public GameObject[] targets;
+	Vector3[] targetStartPositions;
+	public Transform target;
+	public GUIStyle style;
+	public demoButton[] buttons;
+	int i = 0;
+
+	void Start(){
+		//lets fill in the start positions for all the possible targets
+		targetStartPositions = new Vector3[targets.Length];
+		for(i=0;i<targets.Length;i++){
+			targetStartPositions[i] = targets[i].transform.position;
+		}
+		if(target == null){
+			target = targets[0].transform;
+		}
+	}
+
+
+	void OnGUI() {
+		for(i=0;i<buttons.Length;i++){
+			if (buttons[i].isLabel){
+				GUI.Label(buttons[i].btnRect,buttons[i].btnName,style);
+			}else{
+				if (GUI.Button(buttons[i].btnRect,buttons[i].btnName)){
+					if(buttons[i].callFunction){
+						gameObject.SendMessage(buttons[i].btnName);
+					}else{
+						PlayAnim (buttons[i].btnName);
+					}
+				}
+			}
+		}
+	}
+
+	void PlayAnim(string animName){
+		for(i=0;i<targets.Length;i++){
+			targets[i].GetComponent<Animator>().Play(animName,0);
+		}
+	}
+}
