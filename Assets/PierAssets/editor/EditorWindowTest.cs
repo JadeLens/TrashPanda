3af5fd49b3038b059ac06e3e10ad3b079ee5dbde
poboivin <poboivin@hotmail.com>
2016-03-30 09:06:41 -0400

    ...

---
+++ Assets/PierAssets/editor/EditorWindowTest.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+// Create a dockable empty window at the top left corner of the screen
+// with 100px width and 150px height
+public class EditorWindowTest : EditorWindow
+{
+   static Rect myBox;
+    static Color boxColor;
+
+    Rect windowRect = new Rect(100 + 100, 100, 100, 100);
+    Rect windowRect2 = new Rect(100, 100, 100, 100);
+
+    [MenuItem("Example/Display simple sized Window")]
+    static void Initialize()
+    {
+        EditorWindowTest window = (EditorWindowTest)EditorWindow.GetWindowWithRect(typeof(EditorWindowTest), new Rect(0, 0, 400, 400));
+        myBox = new Rect(100, 200, 50, 50);
+        boxColor = Color.red;
+    }
+    void OnGUI()
+    {
+        GUILayout.Label("Base yooo", EditorStyles.boldLabel);
+      boxColor =  EditorGUI.ColorField(new Rect(0, 50, 60, 30), boxColor);
+        GUILayout.Label("adv yooo", EditorStyles.boldLabel);
+        EditorGUI.DrawRect(myBox, boxColor);
+
+        Handles.BeginGUI();
+        Handles.DrawBezier(windowRect.center, windowRect2.center, new Vector2(windowRect.xMax + 50f, windowRect.center.y), new Vector2(windowRect2.xMin - 50f, windowRect2.center.y), Color.red, null, 5f);
+        Handles.EndGUI();
+
+
+        BeginWindows();
+        windowRect = GUI.Window(0, windowRect, WindowFunction, "Box1");
+        windowRect2 = GUI.Window(1, windowRect2, WindowFunction, "Box2");
+
+        EndWindows();
+
+    }
+    void WindowFunction(int windowID)
+    {
+        GUI.DragWindow();
+    }
+}
