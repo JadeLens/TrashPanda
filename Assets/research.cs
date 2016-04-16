d9b43b058b29b6a6897e2f49524f1c45cfb2364e
Ryan Hammond <ryan.hammond33@gmail.com>
2016-04-16 16:47:14 -0400

    added functionality for researching of new units

---
+++ Assets/research.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class research : MonoBehaviour {
+    GameObject timebar;
+    public GameObject rabbitDen;
+    private GameObject m_caller;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void startResearch(GameObject caller)
+    {
+        timebar = caller.transform.GetChild(1).gameObject;
+        timebar.GetComponentInChildren<timer>().StartTimer(caller);
+        m_caller = caller;
+
+    }
+
+    public void finished()
+    {
+        m_caller.GetComponentInChildren<Text>().text = "Raccoon";
+        timebar.SetActive(false);
+
+        //GetComponentInChildren<Text>().text = "Raccoon";
+        m_caller.GetComponent<Button>().onClick.RemoveAllListeners();
+        m_caller.GetComponent<Button>().onClick.AddListener(rabbitDen.GetComponent<UnitBuilder>().SpawnRabbit);
+        //this.GetComponent<research>().enabled = false;
+        //this.GetComponent<Button>()
+
+    }
+
+    
+}
---
+++ Assets/timer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour {
+
+    public GameObject researchHolder;
+    bool running = false;
+    Slider slider;
+	// Use this for initialization
+	void Start () {
+        slider = this.GetComponent<Slider>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if(running == true)
+        {
+            slider.value += Time.deltaTime;
+            if(slider.value >= 10)
+            {
+                researchHolder.GetComponent<research>().finished();
+                //GetComponentInParent<research>().finished();
+            }
+        }
+	}
+
+    public void StartTimer(GameObject caller)
+    {
+        running = true;
+    }
+}
