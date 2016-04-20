54ab7eff54e5319edcbe82b6c764b8977bf03e5b
theonly55 <paul.pews@gmail.com>
2016-04-20 09:43:38 -0400

    Hover, text, other fixes

---
+++ Assets/PierAssets/Hover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Hover : MonoBehaviour {
+
+
+    [SerializeField]
+    GameObject M_Button;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void HoverTrue(GameObject Obj)
+    {
+        Obj.SetActive(true);
+    }
+    public void HoverFalse(GameObject Obj)
+    {
+        Obj.SetActive(false);
+
+    }
+
+    public void CoonButton(GameObject Obj)
+    {
+
+        if (M_Button.GetComponentInChildren<Text>().text == "Raccoon")
+        {
+            Obj.GetComponentInChildren<Text>().text = "Cost: Racoon 75 Trash 30 Water";
+        }
+        Obj.SetActive(true);
+    }
+}
